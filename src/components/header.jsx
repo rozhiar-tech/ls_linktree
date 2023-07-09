904391ab7c128665df7f8fcbde2f779ac39665b3
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-07-09 14:47:49 +0300

    init

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="mt-10 mb-10">
+      <div className="flex justify-center">
+        <h1 className="text-4xl font-bold">Header</h1>
+      </div>    
+    </header>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-blue": "#176286",
+        "brand-orange": "#EF8734",
+      },
+    },
+  },
+  plugins: [],
+};
