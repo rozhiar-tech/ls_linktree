2b114e7fc07c10f877cef7975270c1e242e3e328
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-07-09 15:01:04 +0300

    update

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,11 +1,19 @@
-import React from "react";  
+import React from "react";
 
 const Content = () => {
-    return (
-        <div className="content">
-        <h1>Content</h1>
-        </div>
-    );
+  return (
+    <div className="content flex flex-col justify-center items-center gap-11">
+      <div className="bg-white w-96 h-10 rounded-md flex justify-center items-center">
+        <h1 className="text-center text-brand-orange">Content</h1>
+      </div>
+      <div className="bg-white w-80 h-10 rounded-md">
+        <h1 className="text-center text-brand-orange">Content</h1>
+      </div>
+      <div className="bg-white w-80 h-10 rounded-md">
+        <h1 className="text-center text-brand-orange">Content</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Logo from "./logo.jpg";
 
 const Header = () => {
   return (
-    <header className="mt-10 mb-10">
-      <div className="flex justify-center">
-        <h1 className="text-4xl font-bold">Header</h1>
-      </div>    
+    <header className="mt-20 mb-10">
+      <div className="flex justify-center flex-col items-center">
+        <img src={Logo} alt="Logo" className="w-16" />
+        <h1 className="text-4xl font-bold text-brand-orange">Ls Aqarat</h1>
+      </div>
     </header>
   );
 };
