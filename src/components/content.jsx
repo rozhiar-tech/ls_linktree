e81097891e296f7a78f8f33fdf8633d0ec8cf363
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-07-09 15:54:46 +0300

    update

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,30 +1,68 @@
 import React from "react";
+import Fb from "./facebook.png";
+import Insta from "./instagram.png";
+import Snap from "./snapchat.png";
+import YouTube from "./youtube.png";
+import TikTok from "./tik-tok.png";
 
 const Content = () => {
   return (
     <div className="content flex flex-col justify-center items-center gap-11">
       <div
-        className="bg-white w-80 h-14 rounded-md flex justify-center items-center text-brand-orange font-bold
+        className="bg-white w-80 h-14 rounded-lg flex justify-start items-center text-brand-orange font-bold
        hover:bg-brand-orange hover:text-brand-blue"
       >
-        <a href="https://linktr.ee/expedia" className="text-center ">
-          <h1 className="text-center ">Content</h1>
+        <img className="w-10 h-10 ml-3" src={Fb} alt="Facebook" />
+        <a
+          href="https://www.facebook.com/profile.php?id=100094094267808"
+          className="text-center ml-20"
+        >
+          <h1 className="text-center ">Facebook</h1>
         </a>
       </div>
       <div
-        className="bg-white w-80 h-14 rounded-md flex justify-center items-center text-brand-orange font-bold
+        className="bg-white w-80 h-14 rounded-lg flex justify-start items-center text-brand-orange font-bold
        hover:bg-brand-orange hover:text-brand-blue"
       >
-        <a href="https://linktr.ee/expedia" className="text-center ">
-          <h1 className="text-center ">Content</h1>
+        <img className="w-10 h-10 ml-3" src={Insta} alt="instagram" />
+        <a
+          href="https://www.instagram.com/ls.aqar/"
+          className="text-center ml-20"
+        >
+          <h1 className="text-center ">Instagram</h1>
         </a>
       </div>
       <div
-        className="bg-white w-80 h-14 rounded-md flex justify-center items-center text-brand-orange font-bold
+        className="bg-white w-80 h-14 rounded-lg flex justify-start items-center text-brand-orange font-bold
        hover:bg-brand-orange hover:text-brand-blue"
       >
-        <a href="https://linktr.ee/expedia" className="text-center ">
-          <h1 className="text-center ">Content</h1>
+        <img className="w-10 h-10 ml-3 " src={Snap} alt="Snapchat" />
+        <a
+          href="https://www.snapchat.com/add/ls.aqar?share_id=zvITPPO68fw&locale=en-IQ"
+          className="text-center ml-20"
+        >
+          <h1 className="text-center ">Snapchat</h1>
+        </a>
+      </div>
+      <div
+        className="bg-white w-80 h-14 rounded-lg flex justify-start items-center text-brand-orange font-bold
+       hover:bg-brand-orange hover:text-brand-blue"
+      >
+        <img className="w-10 h-10 ml-3 " src={YouTube} alt="Snapchat" />
+        <a href="https://www.youtube.com/@LSAqar" className="text-center ml-20">
+          <h1 className="text-center ">YouTube</h1>
+        </a>
+      </div>
+      <div
+        className="bg-white w-80 h-14 rounded-lg flex justify-start items-center text-brand-orange font-bold
+       hover:bg-brand-orange hover:text-brand-blue"
+      >
+        <img className="w-10 h-10 ml-3 " src={TikTok} alt="TikTok" />
+        <a
+          href="https://www.tiktok.com/@ls.aqar?lang=en"
+          className="text-center ml-20"
+        >
+          <h1 className="text-center ">TikTok</h1>
         </a>
       </div>
     </div>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,41 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <h1>Footer</h1>
+      <h1 className="text-center text-2xl font-bold text-brand-orange mt-10">
+        You Found it
+      </h1>
+      <div className="flex justify-center items-center gap-5 mt-10">
+        <a
+          href="https://www.facebook.com/profile.php?id=100094094267808"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/fluent/48/000000/facebook-new.png"
+            alt="Facebook"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/ls.aqar/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
+            alt="Instagram"
+          />
+        </a>
+        <a
+          href="https://www.snapchat.com/add/ls.aqar?share_id=zvITPPO68fw&locale=en-IQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/fluent/48/000000/snapchat.png"
+            alt="Snapchat"
+          />
+        </a>
+      </div>
     </footer>
   );
 };
