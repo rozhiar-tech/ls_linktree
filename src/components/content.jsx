3200d0c298ff4f3ca41e0bf26ad2bbdba6003609
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-07-09 15:14:54 +0300

    update

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -3,14 +3,29 @@ import React from "react";
 const Content = () => {
   return (
     <div className="content flex flex-col justify-center items-center gap-11">
-      <div className="bg-white w-96 h-10 rounded-md flex justify-center items-center">
-        <h1 className="text-center text-brand-orange">Content</h1>
+      <div
+        className="bg-white w-80 h-14 rounded-md flex justify-center items-center text-brand-orange font-bold
+       hover:bg-brand-orange hover:text-brand-blue"
+      >
+        <a href="https://linktr.ee/expedia" className="text-center ">
+          <h1 className="text-center ">Content</h1>
+        </a>
       </div>
-      <div className="bg-white w-80 h-10 rounded-md">
-        <h1 className="text-center text-brand-orange">Content</h1>
+      <div
+        className="bg-white w-80 h-14 rounded-md flex justify-center items-center text-brand-orange font-bold
+       hover:bg-brand-orange hover:text-brand-blue"
+      >
+        <a href="https://linktr.ee/expedia" className="text-center ">
+          <h1 className="text-center ">Content</h1>
+        </a>
       </div>
-      <div className="bg-white w-80 h-10 rounded-md">
-        <h1 className="text-center text-brand-orange">Content</h1>
+      <div
+        className="bg-white w-80 h-14 rounded-md flex justify-center items-center text-brand-orange font-bold
+       hover:bg-brand-orange hover:text-brand-blue"
+      >
+        <a href="https://linktr.ee/expedia" className="text-center ">
+          <h1 className="text-center ">Content</h1>
+        </a>
       </div>
     </div>
   );
