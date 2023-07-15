3b0d417e59c73730cde589cc823bf32f5bd29c76
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-07-15 13:08:08 +0300

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Logo from "./logo.jpg";
+import Logo from "./LOGO.png";
 
 const Header = () => {
   return (
     <header className="mt-20 mb-10">
       <div className="flex justify-center flex-col items-center">
-        <img src={Logo} alt="Logo" className="w-16" />
-        <h1 className="text-4xl font-bold text-brand-orange">Ls Aqarat</h1>
+        <img src={Logo} alt="Logo" className="w-40" />
+        {/* <h1 className="text-4xl font-bold text-brand-orange">Ls Aqarat</h1> */}
       </div>
     </header>
   );
